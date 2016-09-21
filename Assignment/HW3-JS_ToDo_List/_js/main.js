b04aa5ebbd940fbb7cf3be54bd48056c88ad0bf4
Paul Glenn <paul.glenn@gmail.com>
2016-09-21 14:06:07 -0700

    starting hw3

--- Assignment/HW3-JS_ToDo_List/_js/main.js
+++ Assignment/HW3-JS_ToDo_List/_js/main.js
@@ -1,13 +1,16 @@
 $(document).ready(
     $("#new-item").on('click', function() {
+        console.log("haiiii");
         // once the document loads, create new item with this function
+        var user_input = $('#todo-item-input').val();
+        // alert(user_input);
+        $('#list_todo').prepend("<li> <button> Move me! </button>" + user_input + "</li>");
     })
 );
 
 $("#list_todo").on('click', "button", function() {
         // move from list_todo container to list_completed container
-});
-
-$("#list_completed").on('click', "button", function() {
-        // move back from list_completed container to list_todo container
-});
+        $(this).html("Add to To-Do");
+        var completedItem = $(this).parent()
+        $("#list_completed").prepend(completedItem);
+    });
