b400b8eac3e827d0ff4fc3b394b370edbd3ea9a8
Robin Oh <Robin Oh>
2016-09-22 11:37:47 -0700

    first draft

--- Assignment/HW3-JS_ToDo_List/_js/main.js
+++ Assignment/HW3-JS_ToDo_List/_js/main.js
@@ -1,13 +1,25 @@
+//main.js created by Robin Oh
+//handles the back and forth between to do listss
+
 $(document).ready(
     $("#new-item").on('click', function() {
         // once the document loads, create new item with this function
+        var user_input = $("#todo-item-input").val();
+        $("#list_todo").prepend("<li> <button> Completed! </button>" + user_input + "</li>");
     })
 );
 
 $("#list_todo").on('click', "button", function() {
         // move from list_todo container to list_completed container
+        $(this).html("Add To do");
+        var completedItem = $(this).parent();
+        $("#list_completed").prepend(completedItem);
 });
 
 $("#list_completed").on('click', "button", function() {
         // move back from list_completed container to list_todo container
+
+        $(this).html("Completed!");
+        var back2Todo = $(this).parent();
+        $("#list_todo").prepend(back2Todo);
 });
