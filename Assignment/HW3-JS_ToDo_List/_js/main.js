9c7e67a39b9fdbd3315920c6c782a77abdf70d53
Paul Glenn <paul.glenn@gmail.com>
2016-09-22 09:51:12 -0700

    Working basic homework, starting EC

--- Assignment/HW3-JS_ToDo_List/_js/main.js
+++ Assignment/HW3-JS_ToDo_List/_js/main.js
@@ -1,16 +1,26 @@
 $(document).ready(
-    $("#new-item").on('click', function() {
+    $(".new-item").on('click', function() {
         console.log("haiiii");
         // once the document loads, create new item with this function
-        var user_input = $('#todo-item-input').val();
+        var user_input = $('.todo-item-input').val();
         // alert(user_input);
-        $('#list_todo').prepend("<li> <button> Move me! </button>" + user_input + "</li>");
+        $('.list_todo').prepend("<li> <button> <img src='complete.png'> </button>" + user_input + "</li>");
     })
 );
 
-$("#list_todo").on('click', "button", function() {
+    $(".list_todo").on('click', "button", function() {
         // move from list_todo container to list_completed container
-        $(this).html("Add to To-Do");
+        $(this).html("<img src='repeat.png'>");
         var completedItem = $(this).parent()
-        $("#list_completed").prepend(completedItem);
+        $(".list_completed").prepend(completedItem);
     });
+    
+
+    $('.list_completed').on('click', "button", function(){
+        $(this).html("<img src='complete.png'>");
+        var completedItem = $(this).parent()
+        $(".list_todo").prepend(completedItem);
+    });
+
+
+
