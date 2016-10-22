2e320ee78c1409ff6c8607a45174d4cde744dec1
Leon Lee <leon.lee@berkeley.edu>
2016-10-22 02:06:47 -0700

    add readme

--- Assignment/HW7 - Mega Lab Travel App/create.sh
+++ Assignment/HW7 - Mega Lab Travel App/create.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-rm -fr migrations
+def init() {
+    rm -fr migrations
+    python3 manage.py drop_db
+    python3 manage.py create_db
+    python3 manage.py db init
+    python3 manage.py db migrate
+}
 
-python3 manage.py drop_db
-python3 manage.py create_db
-python3 manage.py db init
-python3 manage.py db migrate
+init
