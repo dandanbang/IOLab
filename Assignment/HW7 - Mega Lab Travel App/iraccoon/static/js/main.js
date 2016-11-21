e054a6659b62417242d3a7587f0f9a110c3cc9f9
Leon Lee <leon.lee@berkeley.edu>
2016-11-21 14:11:18 -0800

    hw revise

--- Assignment/HW7 - Mega Lab Travel App/iraccoon/static/js/main.js
+++ Assignment/HW7 - Mega Lab Travel App/iraccoon/static/js/main.js
@@ -3,7 +3,7 @@ $(document).on("click", ".btn-danger", function(){
     $(this.parentNode.parentNode).remove();
     trip_name = $(this.parentNode).next().text();
     trip_destination = $(this.parentNode).next().next().text();
-    $.post( "http://0.0.0.0:8000/deletetrip",
+    $.post( "http://0.0.0.0:8001/deletetrip",
             { trip_name: trip_name, trip_destination: trip_destination},
             function(data) {
                 if (data['status'] == 200) {
